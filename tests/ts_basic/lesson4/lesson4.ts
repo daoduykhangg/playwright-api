000bbe01befb1521baa32257ab7c7f5baa674440
Khang Dao <khang.daoduy@yum.com>
2023-10-22 14:13:45 +0700

    introduce TypeScript and type

---
+++ tests/ts_basic/lesson4/lesson4.ts
@@ -0,0 +1,6 @@
+const sum = (x: number, y: number) => {
+  return x + y;
+}
+
+console.log("Check sum = " , sum(21,8));
+// console.log("Check sum = " , sum("21",8));
---
+++ tests/ts_basic/lesson5/lesson5.ts
@@ -0,0 +1,4 @@
+const fullName = 'Dao Duy Khang';
+
+console.log("length = ", fullName.length);
+console.log("toUpperCase = ", fullName.toUpperCase());
